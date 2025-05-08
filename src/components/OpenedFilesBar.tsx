5383651e24680563de783ab6bf0efce85df0d312
diab-tech <shapan.mohamed9779@gmail.com>
2025-05-08 12:03:07 +0300

    دمج فرع newMain مع حل التعارضات

--- src/components/OpenedFilesBar.tsx
+++ src/components/OpenedFilesBar.tsx
@@ -1,33 +1,20 @@
 import { RootState } from '../app/store';
 import { useSelector } from 'react-redux';
 import OpenedFilesTaps from './OpenedFilesTab';
-<<<<<<< HEAD
-=======
 import FileSyntaxHighlighter from './SyntaxHighlighter';
->>>>>>> cceab709ebe8722bd31e978dc552656dcbb832e2
 
 const OpenedFilesBar = () => {
   const { openedFiles, clickedFile } = useSelector((state: RootState) => state.fileTreeSlice);
 
   return (
-<<<<<<< HEAD
-    <div className=" flex flex-col w-full  bg-gray-200">
-      <div className="flex w-full bg-[#252526] gap- h-10">
-=======
     <div className=" flex flex-col w-full  bg-gray-200 ">
       <div className=" flex bg-[#252526] h-10 overflow-x-auto whitespace-nowrap scrollbar-none text-sm cursor-pointer">
->>>>>>> cceab709ebe8722bd31e978dc552656dcbb832e2
         {openedFiles.map((file) => (
           <OpenedFilesTaps file={file} key={file.id} />
         ))}
       </div>
-<<<<<<< HEAD
-      <div className="bg-[#282a36] min-h-[calc(100vh-40px)] text-white">
-        {clickedFile.fileContent}
-=======
       <div className="max-h-[100vh] scroll-auto overflow-y-auto">
         <FileSyntaxHighlighter content={clickedFile.fileContent} />
->>>>>>> cceab709ebe8722bd31e978dc552656dcbb832e2
       </div>
     </div>
   );
--- src/components/RecursiveComponent.tsx
+++ src/components/RecursiveComponent.tsx
@@ -1,10 +1,6 @@
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-<<<<<<< HEAD
-import { setActiveTab, setClickedFile, setOpenedFiles } from '../app/features/fileTreeSlice';
-=======
 import { setClickedFile, setOpenedFiles } from '../app/features/fileTreeSlice';
->>>>>>> cceab709ebe8722bd31e978dc552656dcbb832e2
 import { AppDispatch, RootState } from '../app/store';
 import { IFile } from '../interface';
 import { doesFileExists } from '../utils/functions';
@@ -15,11 +11,7 @@ interface IProps {
 }
 
 const RecursiveComponent = ({ fileTree }: IProps) => {
-<<<<<<< HEAD
-  const { id, isFolder, name, children, content } = fileTree;
-=======
   const { id, name, content } = fileTree;
->>>>>>> cceab709ebe8722bd31e978dc552656dcbb832e2
   const [isOpen, setIsOpen] = useState(false);
   const dispatch = useDispatch<AppDispatch>();
   const { openedFiles } = useSelector((state: RootState) => state.fileTreeSlice);
@@ -27,12 +19,7 @@ const RecursiveComponent = ({ fileTree }: IProps) => {
   // __HANDLERS__
   const onFileClicked = () => {
     const exists = doesFileExists(openedFiles, id);
-<<<<<<< HEAD
-    dispatch(setClickedFile({ fileContent: content, fileName: name }));
-    dispatch(setActiveTab(id));
-=======
     dispatch(setClickedFile({ fileContent: content, fileName: name, activeTab: id }));
->>>>>>> cceab709ebe8722bd31e978dc552656dcbb832e2
     if (exists) return;
     dispatch(setOpenedFiles([...openedFiles, fileTree]));
   };
@@ -46,11 +33,7 @@ const RecursiveComponent = ({ fileTree }: IProps) => {
   return (
     <div className="ml-1">
       <div className="flex items-center cursor-pointer" onClick={toggleFolder}>
-<<<<<<< HEAD
-        <FileIcon fileName={fileTree.name} isFolder={fileTree.isFolder} />
-=======
         <FileIcon fileName={fileTree.name} isFolder={fileTree.isFolder} isOpened={isOpen} />
->>>>>>> cceab709ebe8722bd31e978dc552656dcbb832e2
         {fileTree.isFolder ? (
           <span className="ml-5">{fileTree.name}</span>
         ) : (
